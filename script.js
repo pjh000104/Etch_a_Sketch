b460694e2ea1b20914bb317c0a9e4018b4f22f2a
Jungho Park <122625228+pjh000104@users.noreply.github.com>
2024-02-12 23:46:57 -0500

    added reset button, color pannel, and css styles

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
 const body = document.querySelector('body');
 const container = document.querySelector('#container');
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+const resetBtn = document.querySelector('#resetBtn');
+const colorBlack = 'black';
+const colorWhite = 'White';
+let gridSize = 16;
+makeGrid1();
+draw(colorBlack);
 
-const GridSizeBtn = document.querySelector('#GridSizeBtn');
-const GridSize = 16;
-
-GridSizeBtn.addEventListener('click', () => {
-   GridSize = prompt("abc");
-});
+//https://codepen.io/ElGodzilla06/pen/yLaQXXG
+var colorPicker = document.getElementById("color-picker");
+    var colorValue = document.getElementById("value");
+    colorPicker.onchange = function() {
+        colorValue.innerHTML = colorPicker.value;
+        colorValue.style.color = colorPicker.value;
+        draw(colorPicker.value);
+    }
+//
 
 function makeGrid1() {
-   for(let i = 0; i<16; i++) {
+   for(let i = 0; i<gridSize; i++) {
       const gridBoxColumn = document.createElement('div');
-      gridBoxColumn.classList.add('gridBoxColumn');
-      container.appendChild(gridBoxColumn);
-      for(let j = 0; j<16; j++) {
+      gridBoxColumn.setAttribute('id', 'gridBoxColumn');
+      for(let j = 0; j<gridSize; j++) {
          const gridBoxRow = document.createElement('div');
-         gridBoxRow.classList.add('gridBoxRow');
+         gridBoxRow.setAttribute('id', 'gridBoxRow');
          gridBoxColumn.appendChild(gridBoxRow);
       }
+      container.appendChild(gridBoxColumn);
    }
 }
 
-makeGrid1();
+function draw(color) {
+   let gridBoxes = document.querySelectorAll('#gridBoxRow');
+   for (let i = 0; i < gridSize*gridSize; i++) {
+      gridBoxes[i].addEventListener('mousedown', function() {
+         gridBoxes[i].style.backgroundColor = color;
+      });
+   }
+}
+
+
+gridSizeBtn.addEventListener('click', () => {
+   const rowss = document.querySelectorAll('#gridBoxRow');
+   const columnss = document.querySelectorAll('#gridBoxColumn');
+   for(let i = 0; i<gridSize*gridSize; i++) {
+      rowss[i].remove();
+   }
+   for(let i = 0; i<gridSize; i++) {
+      columnss[i].remove();
+   }
+   gridSize = prompt("Enter Grid Size");
+   makeGrid1();
+   draw(colorPicker.value);
+});
+
+resetBtn.addEventListener('click', () => {
+   let gridBoxes = document.querySelectorAll('#gridBoxRow');
+   for (let i = 0; i < gridSize*gridSize; i++) {
+         gridBoxes[i].style.backgroundColor = 'white';
+   }
+});
 
